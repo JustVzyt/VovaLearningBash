7334a40e21851a829cee5ce87cb3cca6efddf3ff
justvzyt <zakolodnuyvova@gmail.com>
2025-07-20 18:06:36 +0300

    fix ex01

--- Shell01/ex01/print_groups.sh
+++ Shell01/ex01/print_groups.sh
@@ -1,17 +1,2 @@
 FT_USER=justvzyt
-
-# array with users
-arr=()
-for el in $(id -nG $FT_USER)
-do
-arr+=($el)
-done
-
-# print all elements expect the last one
-for el in $(seq 0 $((${#arr[@]} - 2)))
-do
-printf "${arr[$el]},"
-done
-
-# last element
-printf "${arr[$((${#arr[@]} - 1))]}"
+id -nG $FT_USER | tr [:blank:] ,
