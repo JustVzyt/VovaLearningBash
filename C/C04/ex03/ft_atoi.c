73d58c04878f2c244ec350b366cdf797478a985f
justvzyt <zakolodnuyvova@gmail.com>
2025-07-29 15:32:32 +0300

    c04 ex03

--- C/C04/ex03/ft_atoi.c
+++ C/C04/ex03/ft_atoi.c
@@ -6,54 +6,76 @@
 /*   By: justvzyt <justvzyt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 16:09:34 by justvzyt          #+#    #+#             */
-/*   Updated: 2025/07/27 12:09:30 by justvzyt         ###   ########.fr       */
+/*   Updated: 2025/07/29 15:28:38 by justvzyt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	ft_atoi(char *str)
+int	ppow(int num, int power)
 {
-	int		i;
-	int		count_minuses;
-	int		wmp;
-	int		delete_everything_status;
-	int		g;
+	int	product;
+	int	i;
 
-	g = 0;
+	if (power == 0)
+		return (1);
+	product = num;
+	i = 1;
+	while (i++ < power)
+		product *= num;
+	return (product);
+}
+
+int	char_to_int(char *string, int number_count, int count_minuses)
+{
+	int	r;
+	int	i;
+	int	nums;
+
+	r = 0;
 	i = 0;
-	count_minuses = 0;
-	wmp = -1;
-	delete_everything_status = 0;
-	while (str[i] != '\0')
+	nums = 0;
+	while (string[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '+' || delete_everything_status == 1)
+		if (nums < number_count && string[i] >= '0' && string[i] <= '9')
 		{
-			str[i] = '\a';
-		}
-		else if (str[i] != '-' && (str[i] < '0' || str[i] > '9') && str[i] != ' ')
-		{
-			delete_everything_status = 1;
-			str[i] = '\a';
-		}
-		if (str[i] == '-')
-		{
-			if (count_minuses++ > 0)
-				str[i] = '\a';
-			else
-				wmp = i;
+			r += ppow(10, (number_count - nums - 1)) * ((int)string[i] - 48);
+			nums++;
 		}
 		i++;
 	}
-	if (count_minuses % 2 == 0 && wmp != -1)
-		str[wmp] = '\a';
-	printf("%s\n", str);
-	return (*str);
+	if (count_minuses % 2 != 0)
+		r *= -1;
+	return (r);
 }
 
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	counters[2];
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			counters[0]++;
+		}
+		else if (str[i] == '-' || str[i] == '+' || str[i] == ' ')
+		{
+			if (str[i] == '-')
+				counters[1]++;
+		}
+		else
+			break ;
+		//printf("Executed with i = %d; symbol is %c\n", i, str[i]);
+		i++;
+	}
+	return (char_to_int(str, counters[0], counters[1]));
+}
 
 int main(void) {
-	char a[] = "   --+---+1234ab567";
-	printf("%s", ft_atoi(a));
+	char a[] = "12345";
+	printf("Result: %d\n", ft_atoi("12345"));
 	return 0;
 }
